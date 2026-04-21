f2269df753df5a123ef42443190300c26df783ba
Eric Mehiel <emehiel@calpoly.edu>
2026-04-20 21:53:11 -0700

    add gitignore and update test

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import canvasapi as Canvas
 import CPCanvasConnect as cc
+import os
 
-canvas = cc.groups.get_canvas_client()
+api_key = os.getenv("CANVAS_API_KEY")
+canvas = cc.groups.get_canvas_client(api_key=api_key)
 
 print(canvas)
